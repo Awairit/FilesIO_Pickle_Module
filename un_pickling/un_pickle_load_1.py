dfcfb14dc357d31a4047278a553b6588ff74a67b
Mohammad Irshad <awairit2k20@gmail.com>
2025-11-10 15:33:59 +0530

    Loading all dat from Secondary Memory to Main Memory

--- un_pickling/un_pickle_load_1.py
+++ un_pickling/un_pickle_load_1.py
@@ -20,3 +20,27 @@ def read_record():
         print("File not found")
 
 read_record()
+
+#=======================================================================================================================
+
+#Program for read the records from file(emp.pick) by using un-pickling process.
+#EmpUnPickEx2.py
+import pickle
+def readrecords():
+    try:
+        print("-"*50)
+        with open("D:\\Python\\FilesIO_Pickle_Module\\pickling\\emp.pick","rb") as fp:
+            while(True):
+                try:
+                    record = pickle.load(fp)
+                    for val in record:
+                        print("\t{}".format(val),end="\t")
+                    print()
+                except EOFError:
+                    print("-" * 50)
+                    break
+    except FileNotFoundError:
+        print("File Does not Exist")
+
+#Main Program
+readrecords()
